600ecae6f05f25fc167a0a5bea359d228f639fe7
Rafael Lino Lobo <52603609+losfodo@users.noreply.github.com>
2019-11-10 18:23:49 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+//intel - forç - criat - charm
+package pkg05.pkg2mediapessoa;
+
+
+public class Main {
+
+  
+    public static void main(String[] args) {
+     //dinheiro,beleza,personalidade,abilidades
+        float power;
+        float money= 1.1f;
+        float beauty=7f;
+        float personality=7f;
+        float ability=2.2f;
+        power=(money + beauty + personality + ability)/4;
+        System.out.printf("you power is %.2f\n ",power);//usa-se com apenas duas virgulas assim
+        
+        System.out.println("");
+    }
+    
+}
